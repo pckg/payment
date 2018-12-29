325b71ffaa615727e772e7657f14e1646a09c164
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-12-29 01:51:31 +0100

    Paypal

--- src/Pckg/Payment/View/platformSettings/paypal.vue
+++ src/Pckg/Payment/View/platformSettings/paypal.vue
@@ -30,6 +30,23 @@
                     <input type="text" v-model="myPaymentMethod.secret" class="form-control"/>
                 </div>
             </div>
+
+            <!--<div class="form-group">
+                <label>Title</label>
+                <div>
+                    <input type="text" v-model="myPaymentMethod.title" class="form-control"/>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label>Picture</label>
+                <div v-if="!myPaymentMethod.icon">
+                    <img src="/img/payment/paypal.png" class="img-responsive">
+                    <div class="help">You're currently using defaut Comms payment method icon.</div>
+                    <pckg-htmlbuilder-dropzone :current="myPaymentMethod.icon" :url="myPaymentMethod.icon"
+                                               id="dynamic-dropzone-paypal"></pckg-htmlbuilder-dropzone>
+                </div>
+            </div>-->
         </template>
 
         <button type="button" class="btn btn-primary" @click.prevent="saveSettings">Save settigns</button>
