ae762ebf4bba526fd5f112240823ab49c23037cf
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-10-27 12:06:28 +0100

    COD

--- src/Pckg/Payment/Handler/Cod.php
+++ src/Pckg/Payment/Handler/Cod.php
@@ -1,6 +1,48 @@
 <?php namespace Pckg\Payment\Handler;
 
-class Cod extends Proforma
+use Derive\Orders\Record\OrdersBill;
+
+class Cod extends AbstractHandler implements Handler
 {
+
+    /**
+     * Triggered when user submitts "Pay now" button in payment popup.
+     *
+     * @return array|void
+     */
+    public function postStart()
+    {
+        $this->waitForPaymentOnDelivery('COD #' . $this->paymentRecord->id, null, $this->paymentRecord->id);
+
+        return [
+            'success' => true,
+            'modal' => 'cod',
+        ];
+    }
+
+    /**
+     * @param $description
+     * @param $log
+     * @param $transactionId
+     * @param string $status
+     *
+     * Mark payment status as "delivery".
+     */
+    public function waitForPaymentOnDelivery($description, $log, $transactionId, $status = 'delivery')
+    {
+        $this->paymentRecord->addLog($status, $log);
+
+        $this->order->getBills()->keyBy('order_id')->each(function(OrdersBill $ordersBill) use ($description) {
+            /**
+             * Stock is not tracked here.
+             */
+            $ordersBill->order->waitingForPaymentOnDelivery();
+        });
+
+        $this->paymentRecord->setAndSave([
+            'status'         => $status,
+            'transaction_id' => $transactionId,
+        ]);
+    }
     
 }
