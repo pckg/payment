f25ef9a59e336a94b4a9adebb29803dad9f45a23
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-04-03 18:49:39 +0200

    Monri test

--- src/Pckg/Payment/Handler/Monri.php
+++ src/Pckg/Payment/Handler/Monri.php
@@ -89,22 +89,21 @@ class Monri extends AbstractHandler implements Handler
         };
 
         $transactionData = $transactionData('purchase');
-        $url = $this->environment->config('monri.url');
 
-        $data_string = json_encode(['transaction' => $transactionData]);
+        $decoded = $this->callCurl('/v2/transaction', ['transaction' => $transactionData]);
 
-        // Execute transaction
-        $ch = curl_init($url . '/v2/transaction');
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($data_string))
-        );
-        $transaction = curl_exec($ch);
+        if (isset($decoded['secure_message'])) {
+            return [
+                'success' => true,
+                'threeDSecure' => [
+                    'action' => $decoded['secure_message']['acs_url'],
+                    'PaReq' => $decoded['secure_message']['pareq'],
+                    'TermUrl' => null,
+                    'MD' => $decoded['secure_message']['authenticity_token'],
+                ],
+            ];
+        }
 
-        $decoded = json_decode($transaction, true);
         if ($decoded['transaction']['response_message'] === 'approved') {
             $this->approvePayment('Monri #' . $decoded['transaction']['id'], $transaction, $decoded['transaction']['id']);
             return [
@@ -122,8 +121,112 @@ class Monri extends AbstractHandler implements Handler
         ];
     }
 
-    public function postNotification()
+    protected function callCurl($endpoint, $postData)
     {
+        $url = $this->environment->config('monri.url');
+
+        $data_string = json_encode($postData);
+
+        // Execute transaction
+        $ch = curl_init($url . $endpoint);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string))
+        );
+        $transaction = curl_exec($ch);
+
+        return json_decode($transaction, true);
     }
 
+    public function postNotification()
+    {
+        $approvalCode = post('approval_code');
+        $digest = post('digest');
+        $orderNumber = post('order_number');
+        $responseCode = post('response_code');
+
+        // payment should be auto resolved here?
+        if (!$digest || !$approvalCode || !$orderNumber) {
+            return;
+        }
+
+        try {
+            $url = 'https://' . server('SERVER_NAME') . dirname(server('REQUEST_URI')) . '?' . server('QUERY_STRING');
+            $url = parse_url(preg_replace('/&digest=[^&]*/', '', $url));
+            $url = 'https://' . $url['host'] . $url['path'] . '?' . $url['query'];
+            $checkdigest = hash('sha512', $this->environment->config('monri.apiKey') . $url);
+
+            if ($this->paymentRecord->status === 'approved') {
+                $this->paymentRecord->addLog('approved', post()->all());
+                return [
+                    'success' => true,
+                    'issuerCode' => $responseCode,
+                    'note' => 'Already approved',
+                ];
+            }
+
+            if (!$digest !== $checkdigest) {
+                $this->paymentRecord->addLog('error', post()->all());
+                throw new Exception('Digest mismatch');
+            }
+
+            if ($responseCode === "0000") {
+                $this->approvePayment('Monri #', post()->all(), post('approval_code'));
+
+                // for server?
+                return [
+                    'success' => true,
+                    'issuerCode' => $responseCode,
+                ];
+            }
+
+            if ($responseCode === "pending") {
+                $this->paymentRecord->addLog('pending', post()->all());
+
+                return [
+                    'success' => true,
+                    'issuerCode' => $responseCode,
+                ];
+            }
+
+            $this->errorPayment(post()->all());
+
+            return [
+                'success' => false,
+                'issuerCode' => $responseCode,
+            ];
+        } catch (Exception $e) {
+            error_log('MONRI PAYMENT EXCEPTION: ' . exception($e));
+        }
+    }
+
+    /**
+     * Check for valid 3dsecure response
+     **/
+    function postNotification2()
+    {
+        $paRes = post('PaRes');
+        if (!$paRes) {
+            return;
+        }
+
+        $resultXml = $this->callCurl('/pares', ['secure_message' => ['MD' => post('MD'), 'PaRes' => post('PaRes')]]);
+
+        if (($resultXml['status'] ?? null) !== 'approved') {
+            $this->errorPayment($resultXml);
+
+            return [
+                'success' => false,
+            ];
+        }
+
+        $this->approvePayment($resultXml);
+
+        return [
+            'success' => true,
+        ];
+    }
 }
